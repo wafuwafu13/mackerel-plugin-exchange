e8dcad1621547ad7fa6d6a8278622f944c21c503
wafuwafu13 <jaruwafu@gmail.com>
2024-01-06 18:47:39 +0900

    feat: fetch metrics from exchangeratesapi

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module mackerel-plugin-exchange
 
 go 1.21.0
 
-require github.com/mackerelio/go-mackerel-plugin v0.1.4
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/mackerelio/go-mackerel-plugin v0.1.4
+)
 
 require (
 	github.com/mackerelio/golib v1.2.1 // indirect
--- lib/exchange.go
+++ lib/exchange.go
@@ -1,9 +1,18 @@
 package exchange
 
 import (
-	"strings"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
 	mp "github.com/mackerelio/go-mackerel-plugin"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 // ExchangePlugin mackerel plugin
@@ -21,7 +30,7 @@ func (u ExchangePlugin) MetricKeyPrefix() string {
 
 // GraphDefinition interface for mackerelplugin
 func (u ExchangePlugin) GraphDefinition() map[string]mp.Graphs {
-	labelPrefix := strings.Title(u.Prefix)
+	labelPrefix := cases.Title(language.Und, cases.NoLower).String(u.Prefix)
 	return map[string]mp.Graphs{
 		"": {
 			Label: labelPrefix,
@@ -34,9 +43,102 @@ func (u ExchangePlugin) GraphDefinition() map[string]mp.Graphs {
 	}
 }
 
+type ExchangeAPIResponse struct {
+	Success   bool               `json:"success"`
+	Timestamp int                `json:"timestamp"`
+	Base      string             `json:"base"`
+	Date      string             `json:"date"`
+	Rates     map[string]float64 `json:"rates"`
+}
+
 // FetchMetrics interface for mackerelplugin
 func (u ExchangePlugin) FetchMetrics() (map[string]float64, error) {
-	return map[string]float64{"USD": 145.1, "EUR": 158.6}, nil
+	const path = "/Users/wafuwafu13/Desktop/mackerel-plugin-exchange/timestamp.txt"
+	const interval = time.Hour * 2
+	const errorValue = 200.0
+
+	if !isOverTime(path, interval) {
+		return map[string]float64{"USD": errorValue, "EUR": errorValue}, fmt.Errorf("Intentionally not sent")
+	}
+
+	writeTimestamp(path)
+
+	err := godotenv.Load("/Users/wafuwafu13/Desktop/mackerel-plugin-exchange/.env")
+	if err != nil {
+		return map[string]float64{"USD": errorValue, "EUR": errorValue}, fmt.Errorf("godotenv.Load Error: %s", err)
+	}
+
+	apiKey := os.Getenv("EXCHANGE_API_KEY")
+
+	url := fmt.Sprintf("http://api.exchangeratesapi.io/v1/latest?access_key=%s", apiKey)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return map[string]float64{"USD": errorValue, "EUR": errorValue}, fmt.Errorf("http.Get Error: %s", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return map[string]float64{"USD": errorValue, "EUR": errorValue}, fmt.Errorf("io.ReadAll Error: %s", err)
+	}
+
+	var exchangeResponse ExchangeAPIResponse
+	if err := json.Unmarshal(body, &exchangeResponse); err != nil {
+		return map[string]float64{"USD": errorValue, "EUR": errorValue}, fmt.Errorf("json.Unmarshal Error: %s", err)
+	}
+
+	EURToJPY, ok1 := exchangeResponse.Rates["JPY"]
+	EURToUSD, ok2 := exchangeResponse.Rates["USD"]
+	if !ok1 || !ok2 {
+		return map[string]float64{"USD": errorValue, "EUR": errorValue}, fmt.Errorf("ok Error")
+	}
+	USDToJPY := EURToJPY / EURToUSD
+
+	return map[string]float64{"USD": USDToJPY, "EUR": EURToJPY}, nil
+}
+
+// isOverTime checks if the last timestamp in the file is more than interval
+func isOverTime(path string, interval time.Duration) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return false
+	}
+	defer file.Close()
+
+	var lastLine string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lastLine = scanner.Text()
+	}
+
+	if lastLine == "" {
+		return true
+	}
+
+	lastTimestamp, err := time.Parse("2006-01-02 15:04:05", lastLine)
+	if err != nil {
+		fmt.Println("Error parsing timestamp:", err)
+		return false
+	}
+
+	return time.Now().UTC().Sub(lastTimestamp.UTC()) >= interval
+}
+
+// writeTimestamp writes the current timestamp to the file
+func writeTimestamp(path string) {
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(time.Now().UTC().Format("2006-01-02 15:04:05") + "\n")
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+	}
 }
 
 // Do the plugin
